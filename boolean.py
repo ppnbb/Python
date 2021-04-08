c40c2e59aa8a299692deb0da0304c38dcc22ac8a
Hoo Lee <74523555+ppnbb@users.noreply.github.com>
2021-04-08 22:05:17 +0900

    Boolean & Conditional
    test

---
+++ boolean.py
@@ -0,0 +1,19 @@
+#number
+print(1) #Integer
+#String
+print('Hello world')
+#Boolean
+print(True)
+print(False)
+#Expression
+print(1+1)
+print('Hello '+' world')
+#Comparison operator
+print(1==1)
+print(1<2)
+print(2<1)
+#Membership operator
+print('world' in 'Hello world')
+
+import os.path
+print(os.path.exists('boolean.py'))
---
+++ conditional.py
@@ -0,0 +1,17 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_input == '111111':
+    print('Hello master')
+else:
+    print('Who r u?')
+'''
+
+if user_id == 'egoing':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who r u?')
+else:
+    print('Who r u?')
