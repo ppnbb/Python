20d0be64b8e1fda3df92ae85f461a651f56a7841
Hoo Lee <74523555+ppnbb@users.noreply.github.com>
2021-04-10 21:23:46 +0900

    Logical operator
    test

--- conditional.py
+++ conditional.py
@@ -8,10 +8,20 @@ else:
     print('Who r u?')
 '''
 
+'''
 if user_id == 'egoing':
     if user_pwd == '111111':
         print('Hello master')
     else:
         print('Who r u?')
+else:
+    print('Who r u?')
+'''
+
+#logical operator
+if user_id == 'egoing' and user_pwd == '111111':
+    print('Hello master')
+elif user_id == 'leezche' and user_pwd == '222':
+    print('Hello master')
 else:
     print('Who r u?')
