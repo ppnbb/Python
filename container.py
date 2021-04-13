b0d8b934240c04b999764255b747bb43eeecbec9
Hoo Lee <74523555+ppnbb@users.noreply.github.com>
2021-04-13 23:46:02 +0900

    Container
    test

---
+++ container.py
@@ -0,0 +1,21 @@
+#Sequance Types
+a = "a"+"b"
+print(a)
+a = ['a']+['b']
+print(a)
+a = 'egoing'
+print(len(a))
+a = ['a','b','c']
+print(len(a))
+
+#Text Sequance Types(string)
+a = 'egoing'
+print(a.capitalize())
+
+#Mapping Types(dict)
+a = [1,2,3]
+b = ['egoing', 'seoul', 'programmer']
+print(a[0])
+person = {'name':'egoing', 'address':'seoul'}
+print(person['name'])
+print(person['address'])
