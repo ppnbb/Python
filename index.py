8b1f4c40e120a5c81aa68132ed4d977963ea6588
Hoo Lee <74523555+ppnbb@users.noreply.github.com>
2021-04-15 23:31:26 +0900

    os.listdir
    test

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
 #!/usr/local/bin/python3
 print("Content-Type: text/html")
 print()
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
@@ -19,12 +25,10 @@ print('''<!DOCTYPE html>
 <body>
     <h1><a href="index_no_id.py">Web</a></h1>
     <ol>
-        <li><a href="index.py?id=HTML">HTML</a></li>
-        <li><a href="index.py?id=CSS">CSS</a></li>
-        <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+        {listStr}
     </ol>
     <h2>{title}</h2>
     <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId, desc=description))
+'''.format(title=pageId, desc=description, listStr=listStr))
